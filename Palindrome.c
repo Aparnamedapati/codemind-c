24d3bd4419184b433a39094066e7f3449bb81f29
Aparnamedapati <119730429+Aparnamedapati@users.noreply.github.com>
2022-12-08 14:22:50 +0530

    #include<stdio.h> int main() {     int n,t;     scanf("%d",&n);     //number and reverse of the number     int rev=0;t=n;     while(n!=0)     {         rev=rev*10+n%10;         n=n/10;     }     if(t==rev)printf("True");     else printf("False"); }

---
+++ Palindrome.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,t;
+    scanf("%d",&n);
+    //number and reverse of the number
+    int rev=0;t=n;
+    while(n!=0)
+    {
+        rev=rev*10+n%10;
+        n=n/10;
+    }
+    if(t==rev)printf("True");
+    else printf("False");
+}
