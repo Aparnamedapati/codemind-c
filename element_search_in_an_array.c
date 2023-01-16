18733c0cddd222dbcfab04602e0b59d00b462197
Aparnamedapati <119730429+Aparnamedapati@users.noreply.github.com>
2023-01-16 08:00:47 +0530

    #include<stdio.h> int main() {     int i,n,f=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     int k;     scanf("%d",&k);     for(i=0;i<n;i++){         if(a[i]==k){             f=1;             break;         }     }     if(f==1)     printf("True");     else printf("False"); }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,f=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int k;
+    scanf("%d",&k);
+    for(i=0;i<n;i++){
+        if(a[i]==k){
+            f=1;
+            break;
+        }
+    }
+    if(f==1)
+    printf("True");
+    else printf("False");
+}
